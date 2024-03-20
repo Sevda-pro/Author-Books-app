033d4f455ad8ebbb308ec581f18ab9c02ad16819
Sevda-pro <mesahilsevda@gmail.com>
2024-03-20 09:25:14 +0530

    response Handler in Controller is updated

--- Controller/responseHandler.js
+++ Controller/responseHandler.js
@@ -1,3 +1,9 @@
+/**
+ * Handle errors in the applicaton and make an err object
+ * @param {String} errormessage-the error message which is passed as an argument in next 
+ * @param {Number} statuscode- the status code which is passed as an argument in next
+ * @returns error object
+ */
 const errorhandler=(errormessage,statuscode)=>{
     const err = new Error();
     err.status = 'fail';
@@ -5,7 +11,12 @@ const errorhandler=(errormessage,statuscode)=>{
     err.message=errormessage;
     return err;
 }
-
+/**
+ * Handle successful responses in the applicaton and make an response object
+ * @param {String} message-the message which is passed as an argument in res.send 
+ * @param {*} data- the data which is passed as an argument in res.send
+ * @returns response object
+ */
 const successhandler=(message,data)=>{
    let response={ success: true, message: message, user: data }
    return response;
